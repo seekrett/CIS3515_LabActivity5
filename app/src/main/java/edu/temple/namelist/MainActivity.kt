31ce622eae26135fae67bf918bb0c15e1acad484
Yuto <63772196+seekrett@users.noreply.github.com>
2025-02-19 15:35:15 -0500

    fixed app crash

--- app/src/main/java/edu/temple/namelist/MainActivity.kt
+++ app/src/main/java/edu/temple/namelist/MainActivity.kt
@@ -12,7 +12,7 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var names: List<String>
+    lateinit var names: MutableList<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -37,8 +37,13 @@ class MainActivity : AppCompatActivity() {
         }
 
         findViewById<View>(R.id.deleteButton).setOnClickListener {
-            (names as MutableList).removeAt(spinner.selectedItemPosition)
+            names.removeAt(spinner.selectedItemPosition)
             (spinner.adapter as BaseAdapter).notifyDataSetChanged()
+            if (!names.isEmpty()) {
+                nameTextView.text = names[0]
+            } else if (names.isEmpty()) {
+                nameTextView.text = "No more names in list."
+            }
         }
 
     }
